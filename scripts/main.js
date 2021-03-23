acdf65cbb9538ca5979be7d1f100b259627a1c16
Kairat <kai.brv.kg@gmail.com>
2021-03-23 16:24:40 +0600

    replace grid file. added response: 320px 425px 768px

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,21 @@ document.addEventListener('DOMContentLoaded', function() {
         dots: false,
         navContainerClass: 'owl-nav nav-featured',
         navText: ["<span class='fa fa-angle-left circle'></span>", "<span class='fa fa-angle-right circle'></span>"],
+        autoHeight: true,
         responsive:{
             1200:{
                 items: 5,
             },
             768: {
                 items: 3
+            },
+            425: {
+                items: 2,
+                margin: 15
+            },
+            320: {
+                items: 1,
+                margin: 10
             }
         }
     });
